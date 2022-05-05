ecb7cdfa9a6d3d6ba34e40a5cb04387c178b8572
Brett <12437916+colbr@users.noreply.github.com>
2022-05-06 07:58:07 +1200

    feat: add basic theming

--- src/providers/ZeroUIProvider.tsx
+++ src/providers/ZeroUIProvider.tsx
@@ -4,14 +4,14 @@ import { BaseProvider } from "baseui";
 import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
 
-import { customLightTheme } from "./themes";
+import { customDarkTheme } from "./themes";
 
 const engine = new Styletron();
 
 export const ZeroUIProvider: FC = ({ children }) => {
   return (
     <StyletronProvider value={engine}>
-      <BaseProvider theme={customLightTheme}>{children}</BaseProvider>
+      <BaseProvider theme={customDarkTheme}>{children}</BaseProvider>
     </StyletronProvider>
   );
 };
--- src/providers/themes.ts
+++ src/providers/themes.ts
@@ -1,30 +1,65 @@
 import { createDarkTheme, createTheme } from "baseui";
 import type { Theme } from "baseui";
 
-interface ICustomTheme extends Theme {}
+interface ICustomTheme extends Theme {
+  background: string;
+}
 
-const lightTheme = createTheme({
-  primaryFontFamily: "Inter",
-  // primary: "#F127E4", // hot pink
-  // primary50: "#FDEDFC",
-  // primary100: "#FCD3F9",
-  // primary200: "#F89FF3",
-  // primary300: "#F45AEA",
-  // primary400: "#F127E4",
-  // primary500: "#B71DAD",
-  // primary600: "#901788",
-  // primary700: "#600F5B",
-  // accent: "#F127E4",
-});
+const extraColors = {
+  accentblue100: "#52CBFF",
+  accentblue200: "#52AEFF",
+  accentblue300: "#5291FF",
+  accentblue400: "#5274FF",
+  gradientazure25: "linear-gradient(90deg, rgba(82, 87, 255, 0.25) 0%, rgba(82, 203, 255, 0.25) 100%)",
+  gradientazure50: "linear-gradient(90deg, rgba(82, 87, 255, 0.50) 0%, rgba(82, 203, 255, 0.50) 100%)",
+  gradientazure100: "linear-gradient(90deg, rgba(82, 87, 255, 1) 0%, rgba(82, 203, 255, 1) 100%)",
+};
 
-const darkTheme = createDarkTheme({
+/**
+ * Variables which are shared between
+ * light and dark theme
+ */
+const PRIMITIVES = {
   primaryFontFamily: "Inter",
-});
+  accent: "#F127E4",
+  accent50: "#6A3991",
+  accent100: "#472E5B",
+  accent200: "#381C4E",
+  accent300: "#23142F",
+  accent400: "#1A1022",
+  accent500: "#120D16",
+  positive: "#58C573",
+  warning: "#E2DC4F",
+};
+
+const INPUT_OVERRIDES = {
+  colors: {
+    inputFill: "linear-gradient(90deg, rgba(82, 174, 255, 0.1) 0%, rgba(82, 203, 255, 0.1) 100%)",
+    inputFillActive: "linear-gradient(90deg, rgba(82, 174, 255, 0.1) 0%, rgba(82, 203, 255, 0.1) 100%)",
+    inputBorder: extraColors.accentblue100,
+  },
+  borders: {
+    inputBorderRadius: "24px",
+  },
+  sizes: {},
+};
+
+const OVERRIDES = {
+  ...INPUT_OVERRIDES,
+};
+
+const lightTheme = createTheme(PRIMITIVES, OVERRIDES);
+
+export const darkTheme = createDarkTheme(PRIMITIVES, OVERRIDES);
 
 export const customLightTheme: ICustomTheme = {
   ...lightTheme,
+  ...extraColors,
+  background: "#fff",
 };
 
 export const customDarkTheme: ICustomTheme = {
   ...darkTheme,
+  ...extraColors,
+  background: "#0A0A0A",
 };
