b9376a1de56d6e3ba5d841357ffe4060973ac41f
Brett <12437916+colbr@users.noreply.github.com>
2022-05-06 07:58:38 +1200

    feat(post): add props

--- src/Post/Post.stories.tsx
+++ src/Post/Post.stories.tsx
@@ -11,3 +11,11 @@ export default {
 const Template: ComponentStory<typeof Post> = (args) => <Post {...args}>Hello</Post>;
 
 export const Primary = Template.bind({});
+Primary.args = {
+  author: "Frank Wilder",
+  avatar: "https://picsum.photos/seed/avatar/164/164",
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus commodo velit semper dapibus. Sed ac egestas urna, ut lobortis nulla. Nunc placerat iaculis erat, in maximus ligula finibus nec. Nullam dignissim lacus id aliquet suscipit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent ultrices feugiat congue. Nulla blandit felis quis convallis semper. Nam non nunc tempor, ullamcorper sem a, euismod libero. Phasellus porttitor leo sed ex consectetur, vel ornare tortor efficitur. Mauris lacinia mollis sem. Aenean sed lobortis purus. Aliquam facilisis tortor tortor.",
+  title: "Lorem Ipsum",
+  zna: "0://frank.wilder",
+  image: "https://picsum.photos/seed/image/600/600",
+};
