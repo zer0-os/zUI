a2384c98f7cc80d63f7f6ea5ec1e4804d8585a2b
joon.choi0918 <joon.choi0918@gmail.com>
2022-07-21 11:22:34 -0400

    remove knobs plugin and use controles addon

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,80 +1,40 @@
-import React, { ReactElement } from "react";
-import { ComponentMeta } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { boolean, select, text } from "@storybook/addon-knobs";
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import Button from "./";
-import { Card } from "../.storybook";
+import { StoryCard } from "../.storybook";
 
 export default {
   title: "Inputs/Button",
   component: Button
 } as ComponentMeta<typeof Button>;
 
-export const Demo = (): ReactElement => {
-  return (
-    <>
-      <Card title="Primary Button" isContrast>
-        <Button variant="primary">Button</Button>
-        <Button variant="primary" isDisabled>
-          Button
-        </Button>
-        <Button variant="primary" isLoading>
-          Button
-        </Button>
-      </Card>
-      <Card title="Secondary Button" isContrast>
-        <Button variant="secondary">Button</Button>
-        <Button variant="secondary" isDisabled>
-          Button
-        </Button>
-        <Button variant="secondary" isLoading>
-          Button
-        </Button>
-      </Card>
-      <Card title="Negative Button" isContrast>
-        <Button variant="negative">Button</Button>
-        <Button variant="negative" isDisabled>
-          Button
-        </Button>
-        <Button variant="negative" isLoading>
-          Button
-        </Button>
-      </Card>
-      <Card title="Text Button" isContrast>
-        <Button variant="text">Button</Button>
-        <Button variant="text" isDisabled>
-          Button
-        </Button>
-        <Button variant="text" isLoading>
-          Button
-        </Button>
-      </Card>
-    </>
-  );
+const Template: ComponentStory<typeof Button> = (args) => (
+  <StoryCard isContrast>
+    <Button {...args}>Button</Button>
+  </StoryCard>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: "primary"
 };
 
-export const Edit = (): ReactElement => {
-  const buttonVariants: Record<string, "primary" | "secondary" | "negative" | "text"> = {
-    primary: "primary",
-    secondary: "secondary",
-    negative: "negative",
-    text: "text"
-  };
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: "secondary"
+};
 
-  return (
-    <>
-      <Card title="Button with editable props" isContrast>
-        <Button
-          variant={select("variant", buttonVariants, "primary")}
-          onPress={action("onPress")}
-          onPressStart={action("onPressStart")}
-          onPressEnd={action("onPressEnd")}
-          isDisabled={boolean("isDisabled", false)}
-          isLoading={boolean("isLoading", false)}
-        >
-          {text("children", "Button")}
-        </Button>
-      </Card>
-    </>
-  );
+export const Negative = Template.bind({});
+Negative.args = {
+  variant: "negative"
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  isDisabled: true
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  isLoading: true
 };
