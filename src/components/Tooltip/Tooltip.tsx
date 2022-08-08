f0bdc3e6acabd50d52ffcc163a4c95528ff1754f
eric-zero-dev <eric.low.dev@gmail.com>
2022-08-09 00:17:58 +0100

    Delete customization from Tooltip component

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -2,7 +2,6 @@ import type { FC, ReactNode } from 'react';
 
 import React from 'react';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
-import classnames from 'classnames';
 import { CSS_PREFIX } from '../constants';
 import './Tooltip.scss';
 
@@ -15,15 +14,6 @@ export type TooltipProps = {
   side: TooltipPrimitive.TooltipContentProps['side'];
   align: TooltipPrimitive.TooltipContentProps['align'];
   content?: ReactNode;
-  rootProps?: Partial<Omit<TooltipPrimitive.TooltipProps, 'open' | 'defaultOpen' | 'onOpenChange'>>;
-  triggerProps?: Partial<Omit<TooltipPrimitive.TooltipTriggerProps, 'asChild'>>;
-  contentProps?: Partial<Omit<TooltipPrimitive.TooltipContentProps, 'side' | 'align'>>;
-  arrowProps?: Partial<TooltipPrimitive.TooltipArrowProps>;
-  classNames?: {
-    triggerClass?: string;
-    contentClass?: string;
-    arrowClass?: string;
-  };
 };
 
 export const Tooltip: FC<TooltipProps> = ({
@@ -33,45 +23,17 @@ export const Tooltip: FC<TooltipProps> = ({
   side = 'top',
   align = 'center',
   content,
-  rootProps,
-  triggerProps,
-  contentProps,
-  arrowProps,
-  classNames,
   children
 }) => {
-  const { delayDuration = 100, ...restRootProps } = rootProps || {};
-  const { width = 32, height = 16, ...restArrowProps } = arrowProps || {};
-
   return (
-    <TooltipPrimitive.Root
-      open={open}
-      defaultOpen={defaultOpen}
-      onOpenChange={onOpenChange}
-      delayDuration={delayDuration}
-      {...restRootProps}
-    >
-      <TooltipPrimitive.Trigger
-        asChild
-        className={classnames(`${BASE_CLASSNAME}__trigger`, classNames?.triggerClass)}
-        {...triggerProps}
-      >
+    <TooltipPrimitive.Root open={open} defaultOpen={defaultOpen} onOpenChange={onOpenChange} delayDuration={100}>
+      <TooltipPrimitive.Trigger asChild className={`${BASE_CLASSNAME}__trigger`}>
         <span tabIndex={0}>{children}</span>
       </TooltipPrimitive.Trigger>
 
-      <TooltipPrimitive.Content
-        side={side}
-        align={align}
-        className={classnames(`${BASE_CLASSNAME}__content`, classNames?.contentClass)}
-        {...contentProps}
-      >
+      <TooltipPrimitive.Content side={side} align={align} className={`${BASE_CLASSNAME}__content`}>
         {content}
-        <TooltipPrimitive.Arrow
-          className={classnames(`${BASE_CLASSNAME}__arrow`, classNames?.arrowClass)}
-          width={width}
-          height={height}
-          {...restArrowProps}
-        />
+        <TooltipPrimitive.Arrow className={`${BASE_CLASSNAME}__arrow`} width={32} height={16} />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Root>
   );
