eb79f7d319144da00ee67c19bf22af2fcdfa9f1a
joon.choi0918 <joon.choi0918@gmail.com>
2022-08-29 20:53:08 -0400

    add theme switcher in storybook toolbar and reflect in preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { ZUIProvider } from '../src/ZUIProvider';
+import { ThemeEngine, Theme, DEFAULT_THEME } from '../src/components';
 
 export const decorators = [
-  Story => {
+  (Story, context) => {
+    const theme = context.parameters.theme || context.globals.theme;
+
     return (
       <MemoryRouter initialEntries={['/']}>
         <ZUIProvider>
+          <ThemeEngine theme={theme} />
           <Story />
         </ZUIProvider>
       </MemoryRouter>
     );
   }
 ];
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for ZUI components',
+    defaultValue: DEFAULT_THEME,
+    toolbar: {
+      // The icon for the toolbar item
+      icon: 'circlehollow',
+      // Array of options
+      items: [
+        { value: Theme.Light, icon: 'circlehollow', title: Theme.Light },
+        { value: Theme.Dark, icon: 'circle', title: Theme.Dark },
+      ],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+    },
+  },
+};
 
 export const parameters = {
   previewTabs: {
