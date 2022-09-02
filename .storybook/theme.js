5b98a79bca52869256ceddfe44756f10327b9c88
joonchoi0918 <101390821+joonchoi0918@users.noreply.github.com>
2022-09-02 04:53:45 -0400

    feat: add storybook version [skip ci] (#26)
    * add storybook version
    
    * feat: change package version [skip ci]
    
    Co-authored-by: Brett <colbr243@gmail.com>

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,6 +1,7 @@
 import { create } from '@storybook/theming';
+import pkg from "../package.json";
 
 export const theme = create({
   base: 'light',
-  brandTitle: 'ZUI Storybook'
+  brandTitle: `zUI v.${pkg.version}`
 });
