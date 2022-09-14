8228bc49ec08f774e1624d1deb8c575611f7a025
Brett <12437916+colbr@users.noreply.github.com>
2022-09-14 18:43:26 +1200

    lint(card): remove unused variable

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { queryByText, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { Card, CardProps } from './';
 import styles from './Card.module.scss';
