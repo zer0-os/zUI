a0bb1dd0680f6f7ea25a86df79d8520429dbd7c0
dominic <domw30@gmail.com>
2022-09-21 18:04:31 +0100

    fix: resolve lint warnings

--- src/components/LoadingIndicator/LoadingIndicator.test.tsx
+++ src/components/LoadingIndicator/LoadingIndicator.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { render } from '@testing-library/react';
 import { LoadingIndicator, LoadingIndicatorProps } from './LoadingIndicator';
