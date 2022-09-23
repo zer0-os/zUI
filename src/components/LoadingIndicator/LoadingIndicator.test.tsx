90f5b19020df54eac03f40a08bf32b56299ff590
dominic <domw30@gmail.com>
2022-09-23 11:47:03 +0100

    refactor: remove function to render test component and add directly to each test

--- src/components/LoadingIndicator/LoadingIndicator.test.tsx
+++ src/components/LoadingIndicator/LoadingIndicator.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { render } from '@testing-library/react';
-import { LoadingIndicator, LoadingIndicatorProps } from './LoadingIndicator';
+import { LoadingIndicator } from './LoadingIndicator';
 
 enum MockSpinnerPositionType {
   BOTTOM = 'bottom',
@@ -21,26 +21,16 @@ jest.mock('./Spinner', () => ({
   Spinner: () => <div>{MOCK_SPINNER}</div>
 }));
 
-const renderComponent = (props?: LoadingIndicatorProps) =>
-  render(
-    <LoadingIndicator
-      className={MOCK_DEFAULT_PROPS.className}
-      text={MOCK_DEFAULT_PROPS.text}
-      spinnerPosition={MOCK_DEFAULT_PROPS.spinnerPosition}
-      {...props}
-    />
-  );
-
 describe('LoadingIndicator', () => {
   test('should render loading indicator container', () => {
-    const { container } = renderComponent();
+    const { container } = render(<LoadingIndicator {...MOCK_DEFAULT_PROPS} />);
     const loadingIndicatorContainer = container.getElementsByClassName('zui-loading-indicator');
 
     expect(loadingIndicatorContainer.length).toBe(1);
   });
 
   test('should render loading indicator text container and text value', () => {
-    const { container } = renderComponent();
+    const { container } = render(<LoadingIndicator {...MOCK_DEFAULT_PROPS} />);
     const textContainer = container.getElementsByClassName('zui-loading-indicator-text');
 
     expect(textContainer.length).toBe(1);
@@ -48,28 +38,28 @@ describe('LoadingIndicator', () => {
   });
 
   test('should render spinner component', () => {
-    const { getByText } = renderComponent();
+    const { getByText } = render(<LoadingIndicator {...MOCK_DEFAULT_PROPS} />);
 
     expect(getByText(MOCK_SPINNER)).toBeTruthy();
   });
 
   describe('when props are defined', () => {
     test('should apply a custom className to loading indicator container className attribute', () => {
-      const { container } = renderComponent();
+      const { container } = render(<LoadingIndicator {...MOCK_DEFAULT_PROPS} />);
       const loadingIndicatorContainer = container.getElementsByClassName('zui-loading-indicator');
 
       expect(loadingIndicatorContainer[0]).toHaveClass(MOCK_DEFAULT_PROPS.className);
     });
 
     test('should append spinnerPosition type to loading indicator container className attribute', () => {
-      const { container } = renderComponent();
+      const { container } = render(<LoadingIndicator {...MOCK_DEFAULT_PROPS} />);
       const loadingIndicatorContainer = container.getElementsByClassName('zui-loading-indicator');
 
       expect(loadingIndicatorContainer[0]).toHaveClass(`zui-loading-indicator-${MOCK_DEFAULT_PROPS.spinnerPosition}`);
     });
 
     test('should append spinnerPosition type to text container className attribute', () => {
-      const { container } = renderComponent();
+      const { container } = render(<LoadingIndicator {...MOCK_DEFAULT_PROPS} />);
       const textContainer = container.getElementsByClassName('zui-loading-indicator-text');
 
       expect(textContainer[0]).toHaveClass(`zui-loading-indicator-text-${MOCK_DEFAULT_PROPS.spinnerPosition}`);
@@ -78,21 +68,21 @@ describe('LoadingIndicator', () => {
 
   describe('when props are undefined', () => {
     test('should not apply a custom className to loading indicator container', () => {
-      const { container } = renderComponent({ className: undefined });
+      const { container } = render(<LoadingIndicator className={undefined} />);
       const loadingIndicatorContainer = container.getElementsByClassName('zui-loading-indicator');
 
       expect(loadingIndicatorContainer[0]).not.toHaveClass(MOCK_DEFAULT_PROPS.className);
     });
 
     test('should append default "bottom" spinnerPosition type to loading indicator container className attribute', () => {
-      const { container } = renderComponent({ spinnerPosition: undefined });
+      const { container } = render(<LoadingIndicator spinnerPosition={undefined} />);
       const loadingIndicatorContainer = container.getElementsByClassName('zui-loading-indicator');
 
       expect(loadingIndicatorContainer[0]).toHaveClass(`zui-loading-indicator-${MockSpinnerPositionType.BOTTOM}`);
     });
 
     test('should not render text container', () => {
-      const { container } = renderComponent({ text: undefined });
+      const { container } = render(<LoadingIndicator text={undefined} />);
       const loadingIndicatorContainer = container.getElementsByClassName('zui-loading-indicator');
       const textContainer = container.getElementsByClassName('zui-loading-indicator-text');
 
@@ -103,7 +93,7 @@ describe('LoadingIndicator', () => {
 
   describe('when text is defined and spinnerPosition is undefined', () => {
     test('should append "bottom" spinnerPosition type to text container className attribute', () => {
-      const { container } = renderComponent({ spinnerPosition: undefined });
+      const { container } = render(<LoadingIndicator spinnerPosition={undefined} />);
       const textContainer = container.getElementsByClassName('zui-loading-indicator-text');
 
       expect(textContainer[0]).toHaveClass(`zui-loading-indicator-text-${MockSpinnerPositionType.BOTTOM}`);
