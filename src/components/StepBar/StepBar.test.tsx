d1d237b9543c148c6e8a4323a053cfbc99cac204
dominic <domw30@gmail.com>
2022-09-27 20:57:07 +0100

    refactor: WIP set up tests

--- src/components/StepBar/StepBar.test.tsx
+++ src/components/StepBar/StepBar.test.tsx
@@ -1,58 +1,72 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { StepBar, Step, StepBarProps } from '.';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { StepBar, Step } from '.';
+import styles from './StepBar.module.scss';
 
-const mockSteps: Step[] = [
+const mockOnChangeStep = jest.fn();
+
+const MOCK_TABS: Step[] = [
   { id: 'step_1', title: 'Step 1' },
   { id: 'step_2', title: 'Step 2' },
   { id: 'step_3', title: 'Step 3' }
 ];
-const mockOnChangeStep = jest.fn();
 
-const renderComponent = (props: StepBarProps) => {
-  return render(<StepBar {...props} />);
+const DEFAULT_PROPS = {
+  currentStepId: MOCK_TABS[0].id,
+  steps: MOCK_TABS
 };
 
-test('should render 1st step', () => {
-  const { getByText } = renderComponent({ currentStepId: mockSteps[0].id, steps: mockSteps });
-  const step1 = getByText(mockSteps[0].title);
-  expect(step1).toBeInTheDocument();
-});
+describe('<StepBar />', () => {
+  test('should apply StepBar class to the container', () => {
+    const { container } = render(<StepBar {...DEFAULT_PROPS} />);
 
-test('should render 2nd step', () => {
-  const { getByText } = renderComponent({
-    currentStepId: mockSteps[1].id,
-    steps: mockSteps,
-    onChangeStep: mockOnChangeStep
+    expect(container.firstChild).toHaveClass(styles.StepBar);
   });
-  const step1 = getByText(mockSteps[0].title);
-  const step2 = getByText(mockSteps[1].title);
-  expect(step1).toBeInTheDocument();
-  expect(step2).toBeInTheDocument();
 
-  fireEvent.click(step1);
+  test('should apply List class only to the List element', () => {
+    const { container } = render(<StepBar {...DEFAULT_PROPS} />);
+    const list = container.getElementsByClassName(styles.List);
 
-  expect(mockOnChangeStep).toHaveBeenCalledWith(mockSteps[0]);
-});
-
-test('should render 3rd step', () => {
-  const { getByText } = renderComponent({
-    currentStepId: mockSteps[2].id,
-    steps: mockSteps,
-    onChangeStep: mockOnChangeStep
+    expect(list.length).toBe(1);
   });
-  const step1 = getByText(mockSteps[0].title);
-  const step2 = getByText(mockSteps[1].title);
-  const step3 = getByText(mockSteps[2].title);
-  expect(step1).toBeInTheDocument();
-  expect(step2).toBeInTheDocument();
-  expect(step3).toBeInTheDocument();
 
-  fireEvent.click(step2);
+  test('should render 1st step', () => {
+    const { getByText } = render(<StepBar currentStepId={MOCK_TABS[0].id} steps={MOCK_TABS} />);
+    const step1 = getByText(MOCK_TABS[0].title);
+    expect(step1).toBeInTheDocument();
+  });
 
-  expect(mockOnChangeStep).toHaveBeenCalledWith(mockSteps[1]);
+  test('should render 2nd step', () => {
+    render(<StepBar currentStepId={MOCK_TABS[1].id} steps={MOCK_TABS} onChangeStep={mockOnChangeStep} />);
 
-  fireEvent.click(step1);
+    const step1 = screen.getByText(MOCK_TABS[0].title);
+    const step2 = screen.getByText(MOCK_TABS[1].title);
+    expect(step1).toBeInTheDocument();
+    expect(step2).toBeInTheDocument();
 
-  expect(mockOnChangeStep).toHaveBeenCalledWith(mockSteps[0]);
+    fireEvent.click(step1);
+
+    expect(mockOnChangeStep).toHaveBeenCalledWith(MOCK_TABS[0]);
+  });
+
+  test('should render 3rd step', () => {
+    const { getByText } = render(
+      <StepBar currentStepId={MOCK_TABS[2].id} steps={MOCK_TABS} onChangeStep={mockOnChangeStep} />
+    );
+
+    const step1 = getByText(MOCK_TABS[0].title);
+    const step2 = getByText(MOCK_TABS[1].title);
+    const step3 = getByText(MOCK_TABS[2].title);
+    expect(step1).toBeInTheDocument();
+    expect(step2).toBeInTheDocument();
+    expect(step3).toBeInTheDocument();
+
+    fireEvent.click(step2);
+
+    expect(mockOnChangeStep).toHaveBeenCalledWith(MOCK_TABS[1]);
+
+    fireEvent.click(step1);
+
+    expect(mockOnChangeStep).toHaveBeenCalledWith(MOCK_TABS[0]);
+  });
 });
