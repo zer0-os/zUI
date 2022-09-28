286ee9fcd561b997c5984c403942ef7194f3e613
dominic <domw30@gmail.com>
2022-09-28 12:29:47 +0100

    fix: resolve unused var warning

--- src/components/StepBar/StepBar.test.tsx
+++ src/components/StepBar/StepBar.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { StepBar, Step } from '.';
 import styles from './StepBar.module.scss';
 
