58e9ce738c6f4cbe0fba62c8eee10fac91ad0d9a
Brett <colbr243@gmail.com>
2022-09-29 20:35:33 +1300

    test(dropdown-menu): increase coverage (#34)
    * refactor(dropdown-menu): improve readability, improve stories
    
    * refactor(dropdown-menu): remove unused prop
    
    * test(dropdown-menu): improve test coverage
    
    * refactor: remove spread prop forwarding
    
    * test(dropdown-menu): adjust test to expected functionality, remove lint warnings [skip ci]
    
    * test(dropdown-menu): refactor tests
    
    * no version bump [skip ci]

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -2,6 +2,7 @@ import React, { forwardRef, ReactNode } from 'react';
 
 import {
   DropdownMenuProps as RadixUIDropdownMenuProps,
+  DropdownMenuContentProps as RadixUIDropdownMenuContentProps,
   Root as RadixUIDropdownMenuRoot,
   Trigger as RadixUIDropdownMenuTrigger,
   Content as RadixUIDropdownMenuContent,
@@ -13,40 +14,51 @@ import './DropdownMenu.scss';
 import classNames from 'classnames';
 
 export interface DropdownItem {
+  /** Unique ID to use in Array.map() */
   id: string;
+  /** Label to appear in the menu */
   label: string | ReactNode;
+  /** Callback to run when an item is selected */
   onSelect: (event?: Event) => void;
   className?: string;
-  icon?: string | ReactNode;
 }
 
-export interface DropdownMenuProps extends RadixUIDropdownMenuProps {
+export interface DropdownMenuProps {
+  /** List of items to render in the dropdown menu */
   items: DropdownItem[];
-  trigger?: ReactNode | 'triple-dot';
+  /** Clicking this will make the dropdown menu appear */
+  trigger?: ReactNode;
+
+  // from RadixDropdownMenu.DropdownMenuProps
+  open?: RadixUIDropdownMenuProps['open'];
+  defaultOpen?: RadixUIDropdownMenuProps['defaultOpen'];
+  onOpenChange?: RadixUIDropdownMenuProps['onOpenChange'];
 
   // from RadixDropdownMenu.DropdownMenuContentProps
-  side?: 'top' | 'right' | 'bottom' | 'left';
-  alignMenu?: 'start' | 'center' | 'end';
+  side?: RadixUIDropdownMenuContentProps['side'];
+  alignMenu?: RadixUIDropdownMenuContentProps['align'];
 
+  /** Class to apply to the trigger */
   className?: string;
-  menuClassName?: string;
 }
 
 export const DropdownMenu = forwardRef<HTMLDivElement, DropdownMenuProps>(
-  ({ className, alignMenu = 'center', items, trigger = 'triple-dot', side = 'bottom', ...dropdownMenuProps }, ref) => {
+  ({ className, alignMenu = 'center', items, trigger, side = 'bottom', open, defaultOpen, onOpenChange }, ref) => {
+    const triggerElement = !trigger ? <span>...</span> : typeof trigger === 'string' ? <span>{trigger}</span> : trigger;
+
     return (
-      <RadixUIDropdownMenuRoot {...dropdownMenuProps}>
+      <RadixUIDropdownMenuRoot open={open} defaultOpen={defaultOpen} onOpenChange={onOpenChange}>
         <RadixUIDropdownMenuTrigger className={classNames(className, 'zui-dropdown-trigger')}>
-          {trigger === 'triple-dot' ? '...' : trigger}
+          {triggerElement}
         </RadixUIDropdownMenuTrigger>
         <RadixUIDropdownMenuContent className={'zui-dropdown-menu'} align={alignMenu} side={side} ref={ref}>
-          {items.map(i => (
+          {items.map(item => (
             <RadixUIDropdownMenuItem
-              className={classNames('zui-dropdown-item', i.className)}
-              key={i.id}
-              onSelect={(event: Event) => i.onSelect(event)}
+              className={classNames('zui-dropdown-item', item.className)}
+              key={item.id}
+              onSelect={(event: Event) => item.onSelect(event)}
             >
-              {i.label}
+              {item.label}
             </RadixUIDropdownMenuItem>
           ))}
         </RadixUIDropdownMenuContent>
