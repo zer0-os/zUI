a51b7c1f9f36980971f204a4de701f065d040bbe
Brett <12437916+colbr@users.noreply.github.com>
2022-09-30 15:15:32 +1300

    story(grid-card): set up better mock values

--- src/components/GridCard/GridCard.stories.tsx
+++ src/components/GridCard/GridCard.stories.tsx
@@ -26,13 +26,13 @@ export const NFTTemplate = Template.bind({ title: 'NFT Template' });
 NFTTemplate.args = {
   children: (
     <NFT
-      buttonText={'Button'}
-      label={'3291503821059321095832190532'}
+      buttonText={'Bid'}
+      label={'Top Bid ($MOCK)'}
       onClickButton={() => alert('yeah')}
-      primaryText={'1,234.00953021593210953291053215321532155'}
-      secondaryText={'$1,234.0032981683921065839532153215321532102168903'}
-      title={'Title638219638921673921863216'}
-      zna={'network.name.3261.3216.312.632153216631.5321.53215.32153216321'}
+      primaryText={'1,234'}
+      secondaryText={'$1,234.50'}
+      title={'Lorem Ipsum'}
+      zna={'lorem.ipsum'}
     />
   )
 };
