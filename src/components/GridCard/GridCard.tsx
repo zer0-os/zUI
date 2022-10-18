4337c206117b9223742d0c051b8d7843ee17693e
Brett <12437916+colbr@users.noreply.github.com>
2022-10-18 22:50:35 +1300

    feat(grid-card): use Image internally

--- src/components/GridCard/GridCard.tsx
+++ src/components/GridCard/GridCard.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 
 import { AspectRatioProps, Root as AspectRatioRoot } from '@radix-ui/react-aspect-ratio';
 import { Skeleton } from '../Skeleton';
+import { Image } from '../Image';
 
 import classNames from 'classnames';
 import styles from './GridCard.module.scss';
@@ -19,7 +20,11 @@ export const GridCard = ({ aspectRatio = 1, className, children, imageAlt, image
   return (
     <div onClick={onClick} className={classNames(styles.Container, className)}>
       <AspectRatioRoot ratio={aspectRatio} className={styles.ImageContainer}>
-        {imageSrc ? <img src={imageSrc} alt={imageAlt} /> : <Skeleton width={'100%'} height={'100%'} />}
+        {imageSrc ? (
+          <Image className={styles.Image} src={imageSrc} alt={imageAlt} />
+        ) : (
+          <Skeleton width={'100%'} height={'100%'} />
+        )}
       </AspectRatioRoot>
       <div className={styles.Content}>{children}</div>
     </div>
