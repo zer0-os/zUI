34db93288e9449eea69ddb00fb0e2f8bfe822745
Nagarjuna Padala <nagarjuna.klu@gmail.com>
2022-12-16 00:12:24 -0600

    feat(checkbox): updated the types for link

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -7,33 +7,62 @@ import classNames from 'classnames';
 import styles from './Checkbox.module.scss';
 const cx = classNames.bind(styles);
 
-export interface LinkProps {
+interface LinkProps {
   text: string;
-  onClick?: () => void;
 }
+export interface ExternalLink extends LinkProps {
+  href: string;
+  openInNewTab?: boolean;
+}
+
+export interface ButtonLink extends LinkProps {
+  onClickLink: () => void;
+}
+
 export interface CheckboxProps {
   className?: string;
   name: string;
-  text?: string;
   isDisabled?: boolean;
   checked?: boolean;
-  link?: LinkProps;
-  variant?: 'Standard' | 'Advanced';
+  variant?: 'standard' | 'advanced';
   onChange?: (value: boolean) => void;
 }
+
+interface StandardCheckboxLabel extends CheckboxProps {
+  variant: 'standard';
+  text: string;
+  link?: ButtonLink | ExternalLink;
+}
+
+export type StandardCheckboxProps = StandardCheckboxLabel & CheckboxProps;
+
+interface AdvancedCheckboxLabel {
+  variant: 'advanced';
+  title: string;
+  description: string;
+  image: string;
+}
+
+export type AdvancedCheckboxProps = AdvancedCheckboxLabel & CheckboxProps;
 
 export const Checkbox = ({
   className,
   name,
-  text,
-  link,
   isDisabled,
   checked,
-  variant = 'Standard',
-  onChange
-}: CheckboxProps) => {
-  const [isChecked, setIsChecked] = useState(checked);
+  variant = 'standard',
+  onChange,
+  ...rest
+}: CheckboxProps | StandardCheckboxProps | AdvancedCheckboxProps) => {
+  let bodyProps;
+  const commonProps = { isDisabled, name };
+  if (variant === 'standard') {
+    bodyProps = { ...(rest as StandardCheckboxProps), ...commonProps };
+  } else {
+    bodyProps = { ...(rest as AdvancedCheckboxProps), ...commonProps };
+  }
 
+  const [isChecked, setIsChecked] = useState(checked);
   const handleChange = (checkState: boolean) => {
     if (!isDisabled) {
       setIsChecked(checkState);
@@ -42,7 +71,7 @@ export const Checkbox = ({
   };
 
   return (
-    <div className={cx(className, styles.Checkbox, styles[`Checkbox${variant}`])} data-disabled={isDisabled}>
+    <div className={styles.Checkbox} data-disabled={isDisabled} data-variant={variant}>
       <RadixCheckbox.Root
         className={styles.CheckboxRoot}
         data-checked={isChecked}
@@ -55,38 +84,66 @@ export const Checkbox = ({
           <IconCheck size={'0.9rem'} />
         </RadixCheckbox.Indicator>
       </RadixCheckbox.Root>
-      {(text || link) && <Label name={name} link={link} text={text} isDisabled={isDisabled} />}
+      {variant === 'standard' ? (
+        <StandardBody {...(bodyProps as StandardCheckboxProps)} />
+      ) : (
+        <AdvancedBody {...(bodyProps as AdvancedCheckboxProps)} />
+      )}
     </div>
   );
 };
 
 /*******************
- * Label and Link
+ * Advanced Title and Description
  *******************/
+const AdvancedBody = (props: AdvancedCheckboxProps) => {
+  return <></>;
+};
 
-interface LabelProps {
-  text?: string;
-  link?: LinkProps;
-  name: string;
-  isDisabled?: boolean;
-}
-
-const Label = ({ text, link, name, isDisabled }: LabelProps) => {
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (link && link.onClick && !isDisabled) {
-      event.preventDefault();
-      link.onClick();
-    }
-  };
-
+/*******************
+ * Standard Label
+ *******************/
+const StandardBody = (props: StandardCheckboxProps) => {
+  const { text, name, isDisabled, link } = props;
   return (
     <label className={styles.Label} data-link={link && true} htmlFor={name}>
       {text && <div data-label-link={text && link && true}>{text}</div>}
-      {link && (
-        <div onClick={handleClick} className={styles.Link}>
-          {link.text}
-        </div>
-      )}
+      {link && <LinkComponent link={link} isDisabled={isDisabled} />}
     </label>
+  );
+};
+
+/*******************
+ * Link
+ *******************/
+
+interface LinkComponentProps {
+  link: StandardCheckboxProps['link'];
+  isDisabled: StandardCheckboxProps['isDisabled'];
+}
+
+const LinkComponent = ({ link, isDisabled }: LinkComponentProps) => {
+  if ((link as ButtonLink).onClickLink) {
+    let { text, onClickLink } = link as ButtonLink;
+
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+      if (!isDisabled) {
+        event.preventDefault();
+        onClickLink();
+      }
+    };
+    return (
+      <div onClick={handleClick} className={styles.Link}>
+        {text}
+      </div>
+    );
+  }
+
+  let { text, href, openInNewTab = true } = link as ExternalLink;
+
+  return (
+    <a href={href} target={openInNewTab ? '_blank' : '_self'} className={styles.Link}>
+      {text}
+    </a>
   );
 };
