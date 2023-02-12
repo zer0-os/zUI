714fa481100c9e2cf82846903fa5c976bc234468
Nagarjuna Padala <nagarjuna.klu@gmail.com>
2023-02-12 00:21:05 -0600

    feat(avatar): updated the test cases

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -5,6 +5,7 @@ import { IconCurrencyEthereum } from '../Icons';
 
 import { AVATAR_ICON_SIZE } from './AvatarIconSize.constants';
 import styles from './Avatar.module.scss';
+import { Status } from '../Status';
 
 export interface AvatarProps {
   imageURL?: string;
@@ -23,43 +24,26 @@ export const Avatar = ({
   statusType,
   badgeContent
 }: AvatarProps) => {
-  const [avatarName, setAvatarName] = useState('');
-
-  // Used to fetch the first two letter of the name
-  useEffect(() => {
-    if (userFriendlyName) {
-      const userNameArray = userFriendlyName.split(' ');
-      let charFromName = userNameArray[0].substring(0, 1);
-      charFromName += userNameArray.length >= 2 ? userNameArray[1].substring(0, 1) : userNameArray[0].substring(1, 2);
-      setAvatarName(charFromName);
-    }
-  }, [userFriendlyName]);
+  const initials = userFriendlyName && getInitials(userFriendlyName);
 
   return (
     <div className={styles.Avatar} data-type={type} data-size={size} tabIndex={0}>
-      <RadixAvatar.Root className={styles.AvatarRoot}>
-        <RadixAvatar.Image className={styles.AvatarImage} src={imageURL} alt={userFriendlyName} />
-        <RadixAvatar.Fallback className={styles.AvatarFallback} delayMs={600}>
+      <RadixAvatar.Root className={styles.Root}>
+        <RadixAvatar.Image className={styles.Image} src={imageURL} alt="avatar" />
+        <RadixAvatar.Fallback className={styles.Fallback} delayMs={600}>
           {userFriendlyName ? (
-            <div> {avatarName}</div>
+            <div> {initials} </div>
           ) : (
-            <div className={styles.DefaultAvatarIcon}>
+            <div className={styles.DefaultIcon}>
               <IconCurrencyEthereum size={AVATAR_ICON_SIZE[size]} />
             </div>
           )}
         </RadixAvatar.Fallback>
       </RadixAvatar.Root>
-      {size != 'extra small' && statusType && <AvatarStatus statusType={statusType} />}
+      {size != 'extra small' && statusType && <Status className={styles.Status} type={statusType} />}
       {size != 'extra small' && badgeContent && <AvatarBadge badgeContent={badgeContent} />}
     </div>
   );
-};
-
-interface StatusTypeProps {
-  statusType: AvatarProps['statusType'];
-}
-const AvatarStatus = ({ statusType }: StatusTypeProps) => {
-  return <div className={styles.Status} data-status-type={statusType}></div>;
 };
 
 interface StatusBadgeTypeProps {
@@ -68,4 +52,11 @@ interface StatusBadgeTypeProps {
 
 const AvatarBadge = ({ badgeContent }: StatusBadgeTypeProps) => {
   return <div className={styles.Badge}> {badgeContent}</div>;
+};
+
+const getInitials = (userFriendlyName: string): string => {
+  const userNameArray = userFriendlyName.split(' ');
+  let charFromName = userNameArray[0].substring(0, 1);
+  charFromName += userNameArray.length >= 2 ? userNameArray[1].substring(0, 1) : userNameArray[0].substring(1, 2);
+  return charFromName;
 };
