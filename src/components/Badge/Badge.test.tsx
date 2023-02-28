4809c50bf5e8e2082d675ce9038aeb24e3599544
Nagarjuna Padala <nagarjuna.klu@gmail.com>
2023-02-28 17:52:28 -0600

    feat(badge): implemented test cases for badge component

---
+++ src/components/Badge/Badge.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+
+import { Badge, BadgeProps } from './';
+import { IconArrowUpRight } from '../icons';
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+describe('Badge', () => {
+  test('should display text', () => {
+    const { getByText } = render(<Badge content="Hello World" type="text" />);
+    expect(getByText('Hello World')).toBeInTheDocument();
+  });
+
+  test('should display number', () => {
+    const { getByText } = render(<Badge content="9+" type="count" />);
+    expect(getByText('9+')).toBeInTheDocument();
+  });
+
+  test('should display icon', () => {
+    const icon = <IconArrowUpRight size={12} />;
+    const { container } = render(<Badge content={icon} type="icon" />);
+    expect(container.firstChild).toHaveAttribute('data-content-type', 'icon');
+  });
+
+  test('should be in green color', () => {
+    const { container } = render(<Badge content="9+" type="text" color="green" />);
+    expect(container.firstChild).toHaveAttribute('style', 'background-color: green;');
+  });
+
+  test('should be in alert state', () => {
+    const { container } = render(<Badge content="Text" type="text" alertState={true} />);
+    expect(container.firstChild).toHaveAttribute('data-alert-state', 'true');
+  });
+});
