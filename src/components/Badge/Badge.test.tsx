811167da45bd5e16952851e82089b516828d4fa4
Nagarjuna Padala <nagarjuna.klu@gmail.com>
2023-02-28 19:03:49 -0600

    feat(badge): removed unwanted imports

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 
-import { Badge, BadgeProps } from './';
+import { Badge } from './';
 import { IconArrowUpRight } from '../Icons';
 
 beforeEach(() => {
