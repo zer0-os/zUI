e1f84f918fa885644f1d9315e371f7a84fb6b1a3
Nagarjuna Padala <nagarjuna.klu@gmail.com>
2023-03-07 21:21:53 -0600

    feat(Badge): changed alert state as error and added variants

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -4,22 +4,16 @@ import classnames from 'classnames';
 import styles from './Badge.module.scss';
 
 export interface BadgeProps {
-  alertState?: boolean;
   className?: string;
-  color?: string;
   content: ReactNode;
-  type: 'text' | 'count' | 'icon';
+  variant: 'active' | 'busy' | 'idle' | 'offline' | 'unread' | 'error';
+  type: 'icon' | 'number' | 'text';
 }
 
-export const Badge = ({ alertState = false, className, color = '#7F46AA', content, type }: BadgeProps) => {
+export const Badge = ({ className, content, variant, type }: BadgeProps) => {
   return (
-    <div
-      className={classnames(styles.Badge, className)}
-      style={{ backgroundColor: color }}
-      data-alert-state={alertState}
-      data-content-type={type}
-    >
-      {content}
+    <div className={classnames(styles.Badge, className)} data-variant={variant} data-content-type={type}>
+      {type === 'number' && content > 9 ? '9+' : content}
     </div>
   );
 };
