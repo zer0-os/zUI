1b12a133d88a7fb429db71f282e4f37fddf94559
Brett <12437916+colbr@users.noreply.github.com>
2023-03-14 09:54:05 +1300

    refactor(grid-card): clean up file

--- src/components/GridCard/GridCard.tsx
+++ src/components/GridCard/GridCard.tsx
@@ -1,7 +1,6 @@
-import React, { MouseEventHandler, ReactNode } from 'react';
+import React, { forwardRef, MouseEventHandler, ReactNode } from 'react';
 
 import { AspectRatioProps, Root as AspectRatioRoot } from '@radix-ui/react-aspect-ratio';
-import { Skeleton } from '../Skeleton';
 import { Image } from '../Image';
 
 import classNames from 'classnames';
@@ -16,13 +15,15 @@ export interface GridCardProps {
   onClick?: MouseEventHandler<HTMLDivElement>;
 }
 
-export const GridCard = ({ aspectRatio = 1, className, children, imageAlt, imageSrc, onClick }: GridCardProps) => {
-  return (
-    <div onClick={onClick} className={classNames(styles.Container, className)}>
-      <AspectRatioRoot ratio={aspectRatio} className={styles.ImageContainer}>
-        <Image className={styles.Image} src={imageSrc} alt={imageAlt} />
-      </AspectRatioRoot>
-      <div className={styles.Content}>{children}</div>
-    </div>
-  );
-};
+export const GridCard = forwardRef<HTMLDivElement, GridCardProps>(
+  ({ aspectRatio = 1, className, children, imageAlt, imageSrc, onClick }: GridCardProps, ref) => {
+    return (
+      <div onClick={onClick} className={classNames(styles.Container, className)} ref={ref}>
+        <AspectRatioRoot ratio={aspectRatio} className={styles.Aspect}>
+          <Image className={styles.Image} src={imageSrc} alt={imageAlt} />
+        </AspectRatioRoot>
+        <div className={styles.Content}>{children}</div>
+      </div>
+    );
+  }
+);
