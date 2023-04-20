99a932bc306518d7bb8782d06845f9ceee91f2fd
Dale Fukami <dale.fukami@gmail.com>
2023-04-20 12:42:44 -0600

    Fix lint

--- src/components/Input/PasswordInput.test.tsx
+++ src/components/Input/PasswordInput.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { PasswordInput, PasswordInputProps } from './PasswordInput';
 
