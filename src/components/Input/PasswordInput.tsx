605b85e7da45bdefe9f95e7d03bca15ed2792492
Brett <12437916+colbr@users.noreply.github.com>
2023-05-25 17:04:08 +1000

    refactor(password-input): code style Rearrange imports, sort props, arrow function handler

--- src/components/Input/PasswordInput.tsx
+++ src/components/Input/PasswordInput.tsx
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
 
-import { Input, InputProps } from './Input';
-import { IconEye, IconEyeOff } from '../../icons';
-
 import { IconButton } from '../IconButton';
+import { Input, InputProps } from './Input';
+import { IconEye, IconEyeOff } from '../Icons';
 
 export type PasswordInputProps = Omit<InputProps, 'type'>;
 
 export const PasswordInput = (props: PasswordInputProps) => {
   const [isHidden, setIsHidden] = useState(true);
 
-  function toggleVisiblity() {
+  const toggleVisiblity = () => {
     setIsHidden(!isHidden);
-  }
+  };
 
   return (
     <Input
-      {...props}
+      endEnhancer={<IconButton Icon={isHidden ? IconEye : IconEyeOff} onClick={toggleVisiblity} type={'button'} />}
       type={isHidden ? 'password' : 'text'}
-      endEnhancer={<IconButton type={'button'} onClick={toggleVisiblity} Icon={isHidden ? IconEye : IconEyeOff} />}
+      {...props}
     />
   );
 };
