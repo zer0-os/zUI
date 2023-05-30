dead227e89d2f4da30e85c3eb3f8222d01ef8333
colbr <colbr243@gmail.com>
2023-05-30 22:57:27 +1000

    test(tabs-nav): improve coverage (#120)
    * refactor: improve tab nav testing
    
    * refactor: increase coverage
    
    * refactor: test description change
    
    * fix: adjust order test to be stricter
    
    * refactor: use screen rather than destructure
    
    * refactor: code style
    
    ---------
    
    Co-authored-by: dominic <domw30@gmail.com>

--- src/components/Tabs/TabsNav.test.tsx
+++ src/components/Tabs/TabsNav.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
-import { TabsNav, TabNav, TabsNavProps } from '.';
+
+import { cleanup, render, screen } from '@testing-library/react';
+import { TabsNav, TabNav } from '.';
+
+import styles from './Tabs.module.scss';
 
 const MOCK_TABS: TabNav[] = [
   { text: 'Tab 1', to: '/1', replace: true },
@@ -12,6 +15,7 @@ const DEFAULT_PROPS = { tabs: MOCK_TABS, location: MOCK_TABS[0].to };
 const mockLink = jest.fn();
 
 jest.mock('react-router-dom', () => ({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Link: (props: any) => {
     mockLink(props);
     return (
@@ -27,30 +31,98 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-const renderComponent = (props?: TabsNavProps) => render(<TabsNav {...DEFAULT_PROPS} {...props} />);
+describe('<TabsNav />', () => {
+  describe('class names', () => {
+    test('should apply Container class only to the container element', () => {
+      const { container } = render(<TabsNav {...DEFAULT_PROPS} />);
 
-test('should render react-router-dom Link elements with correct class names', () => {
-  const { getAllByTestId } = renderComponent();
-  const links = getAllByTestId('link');
-  expect(links.length).toEqual(MOCK_TABS.length);
-  links.forEach(l => expect(l.classList).toContain('Tab'));
-});
+      expect(container.getElementsByClassName(styles.Container).length).toBe(1);
+    });
 
-test('should apply Selected class name on selected Link', () => {
-  const { container } = renderComponent();
-  const selected = container.getElementsByClassName('Selected');
-  expect(selected.length).toBe(1);
-  expect(selected[0].textContent).toBe(MOCK_TABS[0].text);
-});
+    test('should apply List class only to the List element', () => {
+      const { container } = render(<TabsNav {...DEFAULT_PROPS} />);
 
-test('should apply replace prop to Link', () => {
-  renderComponent({ tabs: [MOCK_TABS[0]] });
-  expect(mockLink).toHaveBeenCalledTimes(1);
-  expect(mockLink).toHaveBeenCalledWith(expect.objectContaining({ replace: true }));
-});
+      expect(container.getElementsByClassName(styles.List).length).toBe(1);
+    });
 
-test('should not apply replace prop to Link by default', () => {
-  renderComponent({ tabs: [MOCK_TABS[1]] });
-  expect(mockLink).toHaveBeenCalledTimes(1);
-  expect(mockLink).toHaveBeenCalledWith(expect.objectContaining({ replace: undefined }));
+    test('should apply Tab class only to the Link elements', () => {
+      const { container } = render(<TabsNav {...DEFAULT_PROPS} />);
+
+      expect(container.getElementsByClassName(styles.Tab).length).toBe(3);
+    });
+
+    test('should apply Selected class on default selected link', () => {
+      const { container } = render(<TabsNav {...DEFAULT_PROPS} />);
+      const selectedLink = container.getElementsByClassName(styles.Selected);
+
+      expect(selectedLink[0]).toHaveTextContent(MOCK_TABS[0].text);
+    });
+
+    test('should apply Selected class only to the selected link', () => {
+      const { container } = render(<TabsNav {...DEFAULT_PROPS} />);
+      const selectedLink = container.getElementsByClassName(styles.Selected);
+
+      expect(selectedLink.length).toBe(1);
+      expect(selectedLink[0]).not.toHaveTextContent(MOCK_TABS[1].text);
+      expect(selectedLink[0]).not.toHaveTextContent(MOCK_TABS[2].text);
+    });
+
+    test('should not apply Selected class on any Link by default', () => {
+      const { container } = render(<TabsNav {...DEFAULT_PROPS} location={undefined} />);
+
+      expect(container.getElementsByClassName(styles.Selected).length).toBe(0);
+    });
+  });
+
+  describe('list', () => {
+    test('should render correct number of links', () => {
+      const { container } = render(<TabsNav {...DEFAULT_PROPS} />);
+      const tab = container.getElementsByClassName(styles.Tab);
+
+      expect(tab.length).toEqual(MOCK_TABS.length);
+    });
+
+    test('should render links in the correct order', () => {
+      const MOCK_TABS: TabNav[] = [
+        { text: 'Tab 1,', to: '/1' },
+        { text: 'Tab 2,', to: '/2' },
+        { text: 'Tab 3', to: '/3' }
+      ];
+
+      const { container } = render(<TabsNav tabs={MOCK_TABS} location={MOCK_TABS[0].to} />);
+
+      expect(container.textContent).toBe('Tab 1,Tab 2,Tab 3');
+    });
+  });
+
+  describe('link/tab', () => {
+    test('should apply replace prop to Link', () => {
+      render(<TabsNav {...DEFAULT_PROPS} tabs={[MOCK_TABS[0]]} />);
+
+      expect(mockLink).toHaveBeenCalledTimes(1);
+      expect(mockLink).toHaveBeenCalledWith(expect.objectContaining({ replace: true }));
+    });
+
+    test('should not apply replace prop to Link by default', () => {
+      render(<TabsNav {...DEFAULT_PROPS} tabs={[MOCK_TABS[1]]} />);
+
+      expect(mockLink).toHaveBeenCalledTimes(1);
+      expect(mockLink).toHaveBeenCalledWith(expect.objectContaining({ replace: undefined }));
+    });
+
+    test('should apply and display tab label text', () => {
+      const { container } = render(<TabsNav {...DEFAULT_PROPS} />);
+      const tab = container.getElementsByClassName(styles.Tab);
+
+      expect(tab[0]).toHaveTextContent(MOCK_TABS[0].text);
+      expect(screen.getByText(MOCK_TABS[0].text)).toBeInTheDocument();
+    });
+
+    test('should apply a path for attribute to', () => {
+      const { container } = render(<TabsNav {...DEFAULT_PROPS} />);
+      const tab = container.getElementsByClassName(styles.Tab);
+
+      expect(tab[0]).toHaveAttribute('to', MOCK_TABS[0].to);
+    });
+  });
 });
