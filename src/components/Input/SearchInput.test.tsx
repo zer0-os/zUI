b0c69e99dd12f8739bf7c3d885fca96c86e4cbff
Brett <12437916+colbr@users.noreply.github.com>
2023-06-15 11:37:48 +1200

    test(search-input): value and onChange passed to Input This is the most critical functionality

---
+++ src/components/Input/SearchInput.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { SearchInput, SearchInputProps } from './SearchInput';
+import { InputProps } from './Input';
+
+let mockInput = jest.fn();
+
+jest.mock('./Input', () => ({
+  Input: (props: InputProps) => {
+    mockInput(props);
+    return <div data-testid={'input'} />;
+  }
+}));
+
+const renderComponent = (props: SearchInputProps) => render(<SearchInput {...props} />);
+
+describe('<SearchInput />', () => {
+  it('should pass important props to Input', () => {
+    const mockOnChange = jest.fn();
+    const mockValue = 'mock-value';
+    renderComponent({ value: mockValue, onChange: mockOnChange });
+    expect(mockInput).toHaveBeenCalledWith(
+      expect.objectContaining({
+        value: mockValue,
+        onChange: mockOnChange
+      })
+    );
+  });
+});
