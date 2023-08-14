cf8f79fe9e199cf4db2ec2e58a7f8abdd81eaa61
DomW <domw30@gmail.com>
2023-08-14 08:53:32 +0100

    fix: icon button sizing and icon sizing to match (#140)

--- src/components/IconButton/IconButton.stories.tsx
+++ src/components/IconButton/IconButton.stories.tsx
@@ -13,8 +13,37 @@ const Template: ComponentStory<typeof IconButton> = args => {
   return <IconButton {...args} />;
 };
 
-export const CloseIcon = Template.bind({ title: 'Close Icon Button' });
-CloseIcon.args = {
+export const CloseIconLarge = Template.bind({ title: 'Close Icon Button' });
+CloseIconLarge.args = {
   onClick: (): void => null,
-  Icon: IconXClose
+  Icon: IconXClose,
+  size: 'large'
+};
+
+export const CloseIconSmall = Template.bind({ title: 'Close Icon Button' });
+CloseIconSmall.args = {
+  onClick: (): void => null,
+  Icon: IconXClose,
+  size: 'small'
+};
+
+export const CloseIconXSmall = Template.bind({ title: 'Close Icon Button' });
+CloseIconXSmall.args = {
+  onClick: (): void => null,
+  Icon: IconXClose,
+  size: 'x-small'
+};
+
+export const CloseIconCustomLarger = Template.bind({ title: 'Close Icon Button' });
+CloseIconCustomLarger.args = {
+  onClick: (): void => null,
+  Icon: IconXClose,
+  size: 68
+};
+
+export const CloseIconCustomSmaller = Template.bind({ title: 'Close Icon Button' });
+CloseIconCustomSmaller.args = {
+  onClick: (): void => null,
+  Icon: IconXClose,
+  size: 12
 };
