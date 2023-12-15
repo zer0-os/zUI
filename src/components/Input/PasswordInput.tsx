e5620d6603ef7a812468f1ab7c54ceefb837c0b2
dominic <domw30@gmail.com>
2023-12-15 11:37:05 +0000

    feat: handle icon size on password input

--- src/components/Input/PasswordInput.tsx
+++ src/components/Input/PasswordInput.tsx
@@ -16,7 +16,12 @@ export const PasswordInput = (props: PasswordInputProps) => {
   return (
     <Input
       endEnhancer={
-        <IconButton Icon={isHidden ? IconEye : IconEyeOff} onClick={toggleVisiblity} type={'button'} size={24} />
+        <IconButton
+          Icon={isHidden ? IconEye : IconEyeOff}
+          onClick={toggleVisiblity}
+          type={'button'}
+          size={props.size === 'large' ? 24 : 20}
+        />
       }
       type={isHidden ? 'password' : 'text'}
       {...props}
