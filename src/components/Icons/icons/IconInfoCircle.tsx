175dc70a9ffe71891dcfb51bf3cfccb5b838e8d9
dominic <domw30@gmail.com>
2024-01-10 13:11:46 +0000

    feat: remove stroke width from info circle icon

--- src/components/Icons/icons/IconInfoCircle.tsx
+++ src/components/Icons/icons/IconInfoCircle.tsx
@@ -32,7 +32,6 @@ export const IconInfoCircle = ({
             <path
               d="M12 8H12.01M12 16.5V11.5M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
               stroke={color}
-              strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
             />
