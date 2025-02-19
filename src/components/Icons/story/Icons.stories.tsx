c275e35cc4de2adbb855f4b71bf13c3585993b58
Brett <12437916+colbr@users.noreply.github.com>
2025-02-19 16:25:01 +1100

    feat: make icons easier to scan

--- src/components/Icons/story/Icons.stories.tsx
+++ src/components/Icons/story/Icons.stories.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import * as Icons from '../';
 import { StoryCard } from '../../.storybook';
 
+import './Icons.story.scss';
+
+const icons = Object.values(Icons);
+
 export const AllIcons = () => (
   <StoryCard isContrast>
-    {Object.values(Icons).map((Icon, index) => (
-      <Icon key={index} />
+    {icons.map((Icon, index) => (
+      <div className={'icon'} key={Object.keys(Icons)[index]}>
+        <p>{Object.keys(Icons)[index]}</p>
+        <Icon key={index} />
+      </div>
     ))}
   </StoryCard>
 );
