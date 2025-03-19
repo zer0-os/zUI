f32198d0155dbbb286d0d33abe0e1d4c2a5fd512
Brett <colbr243@gmail.com>
2025-03-19 16:54:41 +0800

    fix: kebab-case SVG properties (#261)

--- src/components/Icons/icons/IconBold.tsx
+++ src/components/Icons/icons/IconBold.tsx
@@ -18,8 +18,8 @@ export const IconBold = ({
       <AccessibleIcon.Root label={label}>
         <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M5.25 4C5.25 3.58579 5.58579 3.25 6 3.25H14C16.6234 3.25 18.75 5.37665 18.75 8C18.75 9.48828 18.0655 10.8167 16.9942 11.6876C18.6213 12.4413 19.75 14.0889 19.75 16C19.75 18.6234 17.6234 20.75 15 20.75H6C5.58579 20.75 5.25 20.4142 5.25 20V4ZM6.75 12.75V19.25H15C16.7949 19.25 18.25 17.7949 18.25 16C18.25 14.2051 16.7949 12.75 15 12.75H6.75ZM6.75 11.25H14C15.7949 11.25 17.25 9.79493 17.25 8C17.25 6.20507 15.7949 4.75 14 4.75H6.75V11.25Z"
             fill={color}
           />
--- src/components/Icons/icons/IconLeftArrow.tsx
+++ src/components/Icons/icons/IconLeftArrow.tsx
@@ -18,8 +18,8 @@ export const IconLeftArrow = ({
       <AccessibleIcon.Root label={label}>
         <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M12.5303 4.46967C12.8232 4.76256 12.8232 5.23744 12.5303 5.53033L6.81066 11.25H19C19.4142 11.25 19.75 11.5858 19.75 12C19.75 12.4142 19.4142 12.75 19 12.75H6.81066L12.5303 18.4697C12.8232 18.7626 12.8232 19.2374 12.5303 19.5303C12.2374 19.8232 11.7626 19.8232 11.4697 19.5303L4.46967 12.5303C4.17678 12.2374 4.17678 11.7626 4.46967 11.4697L11.4697 4.46967C11.7626 4.17678 12.2374 4.17678 12.5303 4.46967Z"
             fill={color}
           />
